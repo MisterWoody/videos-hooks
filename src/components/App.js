9eb457a718184ea1d74a14d294598bdcb77b8cff
Tony Watkins <tony@llifion.net>
2020-12-19 10:22:31 +0000

    Set up our form submit event handler ensuring we prevent default behaviour (stops page reload on enter) and add a placeholder for when we want to fire the callback passed in from the parent component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-﻿import React from 'react';
+import React from 'react';
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,4 @@
-﻿import React from "react";
+import React from "react";
 
 class SearchBar extends React.Component {
     state = { term: '' };
@@ -7,10 +7,16 @@ class SearchBar extends React.Component {
         this.setState( { term: event.target.value});
     };
     
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        
+        // TODO - make sure to call callback from parent component
+    };
+    
     render() {
         return (
             <div className="search-bar ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Video Search</label>
                         <input type="text" value={this.state.term} onChange={this.onInputChange} />
