158651822bcdb2f5a863a532eb474ebc2c72c095
Tony Watkins <tony@llifion.net>
2020-12-19 15:48:36 +0000

    Setup our basic youtube client utilizing axios and our individually registered API key

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+﻿import axios from 'axios';
+
+const KEY = 'INSERT YOUTUBE API KEY HERE';
+
+export default axios.create ({
+   baseURL: 'https://www.googleapis.com/youtube/v3',
+   params: {
+       part: 'snippet',
+       maxResults: 5,
+       key: KEY
+   } 
+});
