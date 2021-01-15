e7ed06c4258726035fc68b964a62d910a8ed4088
Tony Watkins <tony@llifion.net>
2021-01-15 20:46:51 +0000

    Remove our skeleton class component as the refactoring is technically complete

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,22 +11,17 @@ const SearchBar = ({ onFromSubmit }) => {
         event.preventDefault();
         onFormSubmit(term);
     };
-};
 
-class SearchBar extends React.Component {
-    
-    render() {
-        return (
-            <div className="search-bar ui segment">
-                <form className="ui form" onSubmit={this.onFormSubmit}>
-                    <div className="field">
-                        <label>Video Search</label>
-                        <input type="text" value={this.state.term} onChange={this.onInputChange} />
-                    </div>
-                </form>
-            </div>
-        )
-    }
-}
+    return (
+        <div className="search-bar ui segment">
+            <form className="ui form" onSubmit={onSubmit}>
+                <div className="field">
+                    <label>Video Search</label>
+                    <input type="text" value={term} onChange={onInputChange} />
+                </div>
+            </form>
+        </div>
+    )
+};
 
 export default SearchBar;
