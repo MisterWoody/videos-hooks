c1b4146b5870e368426cceeafe2c89c77f9f7d10
Tony Watkins <tony@llifion.net>
2021-01-15 21:27:56 +0000

    Complete the refactoring by moving the contents of the render method into our functional component returning the relevant jsx and removing any references to the this state

--- src/components/App.js
+++ src/components/App.js
@@ -27,32 +27,22 @@ const App = () => {
     const onVideoSelect = (video) => {
         setSelectedVideo(video);
     };
-};
 
-class App extends React.Component {
-    
-    
-    
-    
-    
-    render() {
-        return (
-            <div className="ui container">
-                <SearchBar onFormSubmit={this.onTermSubmit}/>
-                <div className="ui grid">
-                    <div className="ui row">
-                        <div className="eleven wide column">
-                            <VideoDetail video={this.state.selectedVideo} />    
-                        </div>
-                        <div className="five wide column">
-                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
-                        </div>
+    return (
+        <div className="ui container">
+            <SearchBar onFormSubmit={onTermSubmit}/>
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="eleven wide column">
+                        <VideoDetail video={selectedVideo} />
+                    </div>
+                    <div className="five wide column">
+                        <VideoList onVideoSelect={onVideoSelect} videos={videos} />
                     </div>
                 </div>
-            </div>    
-        );
-        
-    }
-}
+            </div>
+        </div>
+    );
+};
 
 export default App;
