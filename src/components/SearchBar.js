00b408729b0d0df9c3ef2f84464c84f2832dfbe1
Tony Watkins <tony@llifion.net>
2021-01-15 20:50:55 +0000

    As the input change event handler is so short, optionally it can be refactored to an inline arrow function on the input field

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,6 @@ import React, { useState } from "react";
 const SearchBar = ({ onFormSubmit }) => {
     const [term, setTerm] = useState('');
 
-    const onInputChange = (event) => {
-        setTerm(event.target.value);
-    };
-
     const onSubmit = (event) => {
         event.preventDefault();
         onFormSubmit(term);
@@ -17,7 +13,7 @@ const SearchBar = ({ onFormSubmit }) => {
             <form className="ui form" onSubmit={onSubmit}>
                 <div className="field">
                     <label>Video Search</label>
-                    <input type="text" value={term} onChange={onInputChange} />
+                    <input type="text" value={term} onChange={(event) => setTerm(event.target.value)} />
                 </div>
             </form>
         </div>
